7656fdb08563c1f1073d9f7676ad885340474ae2
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-07-27 21:29:15 -0300

    Criação do projeto

---
+++ Backend/src/main/java/edu/encurtaUrl/EncurtaUrlApplication.java
@@ -0,0 +1,13 @@
+package edu.encurtaUrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaUrlApplication.class, args);
+	}
+
+}
