239d29eeaba68a79ac31ac4b9177e0d52d1dc96e
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-09-08 00:08:29 -0300

    Melhor forma de se fazer o primeiro teste de integração

--- Backend/src/test/java/edu/encurtaUrl/integration/UrlRoutineTest.java
+++ Backend/src/test/java/edu/encurtaUrl/integration/UrlRoutineTest.java
@@ -3,10 +3,7 @@ package edu.encurtaUrl.integration;
 
 import edu.encurtaUrl.controller.UrlController;
 import edu.encurtaUrl.model.UserBa;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
@@ -14,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 import tools.jackson.databind.ObjectMapper;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -37,36 +36,52 @@ public class UrlRoutineTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+
+    private UserBa userBa;
+
+    @BeforeEach
+    void setUp() {
+        userBa = new UserBa("Bruno", "Bruno@gmail.com", "Bruno2310", BigDecimal.TEN);
+    }
+
     @Nested
-    class noFilter{
+    class Mock{
+
+        private record ShortUrl(String ShortenedURL){};
+
+        ShortUrl wrapperShortUrl(String json){
+            return objectMapper.readValue(json, ShortUrl.class);
+        }
 
         @Test
         @DisplayName("Must generate a short url successfully")
-        void createShortUriTest () throws Exception {
-            UserBa userBa = new UserBa("Bruno", "Bruno@gmail.com", "Bruno2310", BigDecimal.TEN);
-
-            String contentAsString = mockMvc.perform(
+        void createShortUriTest () throws Exception{
+            // todo Esse mockMvc aqui é temp, a ideia desse @Nested é eu usar o RestTemplate
+            String json = mockMvc.perform(
                             post("/encUrl")
                                     .contentType(MediaType.APPLICATION_JSON)
-                                    .content(
-                                            """
-                                                    {
-                                                    "originalUri":"www.google.com"
-                                                    }
-                                                    """
-                                    )
+                                    .content(objectMapper.writeValueAsString(Map.of("originalUri", "www.google.com")))
                                     .with(SecurityMockMvcRequestPostProcessors.user(userBa))
                     ).andExpect(status().isOk())
-                    .andReturn().getResponse().getContentAsString();
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
-            HashMap<String, Object> o = objectMapper.readValue(contentAsString, HashMap.class);
+            ShortUrl shortUrl = wrapperShortUrl(json);
 
-            Assertions.assertAll(
-                    () -> Assertions.assertFalse(o.isEmpty()),
-                    () -> Assertions.assertTrue(o.containsKey("Shortened URL")),
-                    () -> Assertions.assertFalse(o.get("Shortened URL").toString().isBlank())
-            );
+            Assertions.assertFalse(shortUrl.ShortenedURL().isBlank());
+        }
+    }
 
+    @Nested
+    class ResTemp{
+
+        @Test
+        @DisplayName("Must throw an invalid url exception")
+        void mustThrowAnInvalidUrlException() throws Exception{
 
         }
     }
